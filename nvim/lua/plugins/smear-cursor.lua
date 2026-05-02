c8853eea7d39a57e5875c40f4e02b9881452bfec
Ibrahim Amin <ibrahimnasr393@gmail.com>
2026-05-03 02:12:09 +0300

    plugins: add smear-cursor and koda theme

---
+++ nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			cursor_color = "#ffffff",
+
+			never_draw_over_target = true,
+
+			smear_insert_mode = false,
+			min_vertical_distance_smear = 2,
+			min_horizontal_distance_smear = 2,
+
+			time_interval = 17, -- ms
+			stiffness = 0.9,
+			trailing_stiffness = 0.4,
+			damping = 0.99, -- stops bouncing
+		},
+	},
+}
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,23 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "hard",
-        transparent_mode = true,
-      })
-
-      vim.cmd.colorscheme("gruvbox")
-    end,
-  },
-};
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			-- require("gruvbox").setup({
+			--   contrast = "hard",
+			--   transparent_mode = true,
+			-- })
+			--
+			-- vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"oskarnurm/koda.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("koda").setup({ transparent = true })
+			vim.cmd.colorscheme("koda")
+		end,
+	},
+}
